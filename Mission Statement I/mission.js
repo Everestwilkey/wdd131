eb3d6ea5973e1e079a6e95e0cc9298bb353fae9f
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2024-05-03 00:12:49 -0600

    added white logo and center the select dropdown

--- Mission Statement I/mission.js
+++ Mission Statement I/mission.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const image = document.getElementById('byuimg');
   var themeSelector = document.getElementById("theme");
   themeSelector.addEventListener("change", function () {
     // Check if the selected theme is 'dark'
     if (this.value === "dark") {
       document.body.classList.add("dark");
+      image.src = 'images/byui-logo_white.png';
     } else {
       document.body.classList.remove("dark");
+      image.src = 'images/byui-logo_blue.webp';
     }
   });
 });
