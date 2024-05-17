4b0f0edc6e41b28ebf0d5a0bda152c052310a4aa
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2024-05-17 09:55:32 -0600

    cool pics part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -10,13 +10,48 @@ function toggle(){
     }
 
 }
-document.getElementById('menubutton').addEventListener('click',toggle);
+document.querySelector('.menubutton').addEventListener('click',toggle);
 
-// function handleResize(){
-//     const menu = document.getElementById("menubutton")
-//     if(window.innerWidth > 1000){
-//         menu.classList.remove("")
-//     }
+function handleResize() {
+    const menu = document.querySelector(".menubutton");
+    if (window.innerWidth < 1000) {
+        menu.classList.remove('display');
+    } else {
+        menu.classList.add("display");
+    }
+    console.log()
+}
 
-// }
-// window.addEventListener("resize",handleResize)
+// Add an event listener to the window resize event
+window.addEventListener("resize", handleResize);
+
+// Call handleResize once on page load to set the initial state
+handleResize();
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+      <button class="close-viewer">X</button>
+      <img src="${pic}" alt="${alt}">
+      </div>`;
+  }
+
+function viewHandler(event) {
+	// create a variable to hold the element that was clicked on from event.target
+    var imgselect = event.target
+    var altselect = imgselect
+	// get the src attribute from that element and 'split' it on the "-"
+    var img = imgselect.src.split('-')
+    var alt = altselect.alt
+    console.log(alt)
+	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    var fullimg = img[0] + "-full.jpeg"	
+    // insert the viewerTemplate into the top of the body element
+    document.querySelector('body').insertAdjacentHTML("afterbegin", viewerTemplate(fullimg,alt))
+
+
+	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    document.querySelector('.close-viewer').addEventListener('click',() =>{
+        document.querySelector(".viewer").remove()
+    })
+  }
+document.querySelector('.gallery').addEventListener("click",viewHandler)
